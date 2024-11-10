fdd833eb73703782be5584e15cc7dafad1295af8
Sean Davran <seandavran@Seans-MacBook-Air.local>
2024-11-10 01:49:35 -0800

    i am exposing the api key, it does not pose any risks

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 CHUNK_SIZE = 5 # Chunk size in terms of pages.
 K = 5 # How many documents to retrieve?
 MCL = 5 # Maximum context length.
-API_KEY = "your-api-key-here"
+API_KEY = "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"
 
 import openai
 openai.api_key = API_KEY
